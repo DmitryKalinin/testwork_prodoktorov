48f6c413d911dcb834aec544f743c4578d193404
DmitryKalinin <kalinin.dmitriy.1996@gmail.com>
2021-07-14 11:43:54 +0700

    Добавлены комментарии к функциям, сделано сворачивание списков и показ названия изображения

--- main.js
+++ main.js
@@ -2,100 +2,120 @@ let catalog = document.getElementById("catalog");
 let favorite = document.getElementById("favorite");
 let popup = document.getElementById("popup");
 let hiddenState = {
-    catalog: false,
-    name: false,
-    albums: false,
-}
-const getData = async(url, func) => {
+        catalog: false
+    }
+    //--Запуск процедуры отрисовки имен--//
+const main = async(url, func) => {
     let data;
-    let response = await fetch(url);
+    let response = await fetch("https://json.medrating.org/users/");
     await response.json().then(res => { data = res });
-    func(data);
+    show(data);
 }
 
-const main = () => {
-        getData("https://json.medrating.org/users/", show);
-    }
-    //------  Блок создания элементов ------//
+//------  Блок создания элементов ------//
+//--звезда--//
 const starElement = (image) => {
-    let star = document.createElement("img");
-    if (localStorage.getItem(`${image.id}`) === null) {
-        star.setAttribute("src", "./star-svg.svg");
-    } else {
-        star.setAttribute("src", "./star-enable.svg");
+        let star = document.createElement("img");
+        if (localStorage.getItem(`${image.id}`) === null) {
+            star.setAttribute("src", "./star-svg.svg");
+        } else {
+            star.setAttribute("src", "./star-enable.svg");
+        }
+        star.setAttribute("width", "15px");
+        star.classList.add("star");
+        return star;
     }
-    star.setAttribute("width", "15px");
-    star.classList.add("star");
-    return star;
-}
+    //--изображение--//
 const imageElement = (image, width) => {
     let img = document.createElement("img");
     img.setAttribute("src", image.url);
     img.setAttribute("width", width);
     return img
 }
+const popupText = (text) => {
+    let titleImage = document.createElement("span");
+    titleImage.classList.add("popup-text");
+    titleImage.textContent = text;
+    return titleImage;
+}
 
 //------  Блок обработчиков событий ------//
+//--добавление-удаление изображения из localStorage--//
 const addImagetoLocalStorage = (star, image) => {
-    console.log("ДОБАВЛЕНИЕ КАРТИНКИ В LOCALSTORAGE");
-    if (localStorage.getItem(`${image.id}`) === null) {
-        localStorage.setItem(`${image.id}`, JSON.stringify(image));
-        star.setAttribute("src", "./star-enable.svg");
-    } else {
-        localStorage.removeItem(`${image.id}`);
-        star.setAttribute("src", "./star-svg.svg");
-    }
-}
-const showPopup = (image) => {
-    console.log("КЛИК ПО КАРТИНКЕ");
-    popup.innerHTML = "";
-    popup.style.display = "block";
-    let img = document.createElement('img');
-    img.setAttribute("src", image.url);
-    popup.appendChild(img);
-    popup.style.display = "block";
-    popup.addEventListener("click", () => {
-        popup.style.display = "none";
-    })
-}
-
-const show = (data) => {
-    hiddenState.catalog = !hiddenState.catalog;
-    hiddenState.catalog ? catalog.style.display = "block" : catalog.style.display = "none";
-    console.log(catalog.childNodes);
-    data.map(el => {
-        if (el.name && el.name !== undefined) {
-            let catalogNewName = document.createElement('div');
-            catalogNewName.classList.add("catalog-name");
-            catalog.appendChild(catalogNewName);
-            let newName = document.createElement("h2");
-            newName.textContent = el.name;
-            catalogNewName.appendChild(newName);
-            newName.addEventListener("click", () => { showAlbums(catalogNewName, newName, el.id) });
+        console.log("ДОБАВЛЕНИЕ КАРТИНКИ В LOCALSTORAGE");
+        if (localStorage.getItem(`${image.id}`) === null) {
+            localStorage.setItem(`${image.id}`, JSON.stringify(image));
+            star.setAttribute("src", "./star-enable.svg");
+        } else {
+            localStorage.removeItem(`${image.id}`);
+            star.setAttribute("src", "./star-svg.svg");
         }
-    })
-}
-
+    }
+    //--показ полноразмерного изображения--//
+const showPopup = (image) => {
+        console.log("КЛИК ПО КАРТИНКЕ");
+        popup.innerHTML = "";
+        popup.style.display = "block";
+        let img = document.createElement('img');
+        img.setAttribute("src", image.url);
+        popup.appendChild(img);
+        popup.style.display = "block";
+        popup.addEventListener("click", () => {
+            popup.style.display = "none";
+        })
+    }
+    //--процедура отрисовки имен, обработка клика и вызов отрисовки альбомов--//
+const show = (data) => {
+        hiddenState.catalog = !hiddenState.catalog;
+        hiddenState.catalog ? catalog.style.display = "block" : catalog.style.display = "none";
+        console.log(catalog.childNodes);
+        data.map(el => {
+            if (el.name && el.name !== undefined) {
+                let catalogNewName = document.createElement('div');
+                catalogNewName.classList.add("catalog-name");
+                catalog.appendChild(catalogNewName);
+                let newName = document.createElement("h2");
+                newName.textContent = el.name;
+                catalogNewName.appendChild(newName);
+                newName.addEventListener("click", () => {
+                    if (catalogNewName.childNodes.length > 1) {
+                        for (let i = 1; i < catalogNewName.childNodes.length; i++) {
+                            catalogNewName.childNodes[i].remove();
+                        }
+                    } else showAlbums(catalogNewName, newName, el.id);
+                });
+            }
+        })
+    }
+    //--отрисовка названий альбомов, обработка клика и вызов отрисовки изображенеий--//
 const showAlbums = async(catalog, element, id) => {
-    hiddenState.albums = !hiddenState.albums;
+
     let response = await fetch(`https://json.medrating.org/albums?userId=${id}`);
     let albums = await response.json();
     let catalogAlbum = document.createElement('div');
     catalogAlbum.classList.add("catalog-album");
     catalog.appendChild(catalogAlbum);
     console.log('КЛИК ПО ИМЕНИ');
-    hiddenState.albums === false ? catalog.remove() :
-        albums.map(alb => {
-            let album = document.createElement('div');
-            album.classList.add("album");
-            catalogAlbum.appendChild(album);
-            let albumTitle = document.createElement('h3');
-            albumTitle.classList.add("album-title");
-            albumTitle.innerHTML = alb.title;
-            album.appendChild(albumTitle);
-            albumTitle.addEventListener("click", () => { showImage(album, albumTitle, alb.id) });
-        })
+    albums.map(alb => {
+        let album = document.createElement('div');
+        album.classList.add("album");
+        catalogAlbum.appendChild(album);
+        let albumTitle = document.createElement('h3');
+        albumTitle.classList.add("album-title");
+        albumTitle.innerHTML = alb.title;
+        album.appendChild(albumTitle);
+        albumTitle.addEventListener("click", () => {
+            if (album.childNodes.length > 1) {
+                for (let i = 1; i < album.childNodes.length; i++) {
+                    album.childNodes[i].remove();
+                }
+            } else showImage(album, albumTitle, alb.id)
+        });
+
+    })
 }
+
+//--отрисовка изображений, обработки кликов по изображению и звезде--//
 const showImage = async(catalog, element, id) => {
 
     let response = await fetch(`https://json.medrating.org/photos?albumId=${id}`);
@@ -111,21 +131,29 @@ const showImage = async(catalog, element, id) => {
         catalogImages.appendChild(imageItem);
         let star = starElement(image);
         let img = imageElement(image, "150px");
+        let titleImage = popupText(image.title)
         imageItem.appendChild(star)
         imageItem.appendChild(img)
+        imageItem.appendChild(titleImage)
 
         img.addEventListener("click", () => {
             showPopup(image)
         })
-        img.addEventListener("mousemove", () => {
-            console.log("НАВЕДЕНИЕ НА КАРТИНКУ");
+        img.addEventListener("mouseover", () => {
+            titleImage.style.display = "block";
+            img.addEventListener("mouseout", () => {
+                titleImage.style.display = "none";
+            })
+
         })
         star.addEventListener("click", () => {
             addImagetoLocalStorage(star, image);
         })
+
     })
 }
 
+//--показ добавленных в избранное-localStorage изображений--//
 const showFavorite = () => {
     favorite.innerHTML = '';
     let keys = Object.keys(localStorage);
@@ -146,6 +174,7 @@ const showFavorite = () => {
     })
 }
 
+//--обработка кнопок меню--//
 document.getElementById("menu1").addEventListener("click", async() => {
     favorite.style.display = "none";
     catalog.style.display = "block"
